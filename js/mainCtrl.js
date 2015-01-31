c5d2e2ea89fdec8d760ae81c1796abbc1486b808
Stephen Done <srdone@gmail.com>
2015-01-31 11:06:36 -0700

    Completed step 5

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -2,4 +2,8 @@ var app = angular.module('friendsList');
 
 app.controller('mainCtrl', function($scope) {
   $scope.friends = ['Tiffany', 'Jed'];
+  $scope.addFriend = function () {
+    $scope.friends.push($scope.newFriend);
+    $scope.newFriend = null;
+  };
 });
